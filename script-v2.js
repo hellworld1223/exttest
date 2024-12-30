aad33e6427988cbb264364336f3c2e07a3ecd32d
hellworld1223 <125741352+hellworld1223@users.noreply.github.com>
2024-12-30 09:36:09 +0900

    Delete script-v2.js

--- script-v2.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    function printHelloWorld() {
-        console.log("Hello world from script hosted on github repo and delivered through jsDelivr");
-    }
-    printHelloWorld()
-})();
